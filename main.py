4cd1895bdf45ec990c3a219f3ec2f671aab51de8
idris01 <idrys01@gmail.com>
2022-11-04 14:56:32 +0000

    Implements bonus task

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify,request
 import os
+import re
 from dotenv import load_dotenv
 from flask_cors import CORS
-from utility import operands, calculate
+from utility import operands, calculate, response
     
 load_dotenv()
 
@@ -25,29 +26,55 @@ def home():
             age=31,
             bio="Hi, I am a pationate Data Scientist and Associate Cloud Engineer")
         return jsonify(data)
+    
     else:
         data = request.get_json()
         operation = data['operation_type']
         x = float(data['x'])
         y = float(data['y'])
         
-        answer = ...
-        
+        answer = None
+        operation_type = None
+
         if len(operation.strip()) == 1:
             for item,options in operands.items():
                 if operation == options[0]:
                     answer = calculate(x,y,operation)
+                    operation_type = item
                     break
-        else:
-            for items in operands.values():
+        elif len(operation.strip().split()) == 1:
+            for name, items in operands.items():
                 items = list(items)
                 if str(operation[:3]).lower() in ''.join(items):
                     answer = calculate(x,y,items[0])
+                    operation_type = name
                     break
+        else:
+            bot_response = response(operation).choices[0].text
+            pattern = '\d+,?\d+'
+            try:
+                answer = re.findall(pattern,bot_response)
+                if answer:
+                    answer = int(answer[-1].replace(',',''))
+                    for name, items in operands.items():
+                        items = list(items)[1:]
+                        for item in items:
+                            if item.lower()[:3] in bot_response.lower():
+                                operation_type = name
+                                break
+                        if operation_type:
+                            break
+                else:
+                    answer = 0
+                    operation_type = 'Unknown'
+            except TypeError:
+                operation_type='error'
+                answer = 0
+
         result=dict(
                 slackUsername="Idris Adebowale",
                 result=int(answer),
-                operation_type=data['operation_type']
+                operation_type=operation_type
                 )
         return jsonify(result)
                     
--- utility.py
+++ utility.py
@@ -1,7 +1,13 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
 operands = dict(
-        multiply = ['*','multiply','product','times'],
-        add = ['+', 'add', 'sum'],
-        sub = ['-', 'subtract', 'minus']
+        multiplication= ['*','multiply','product','times'],
+        addition = ['+', 'add', 'sum','increment'],
+        subtraction = ['-', 'subtract', 'minus','deduction','decrement']
         )
 
 def calculate(x,y,operand):
@@ -11,3 +17,29 @@ def calculate(x,y,operand):
         return x+y
     else:
         return x-y
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+get_message = lambda message: """
+Answer the following questions.
+Q: the result of deducting 5 from 3 is
+A: the deduction of 5 from 3 is -2
+Q: the sum of 5 and 6 is what
+A: the sum of 5 and 6 is 11
+Q: what is the product of 7 and 11
+A: the product of 7 and 11 is 77
+Q: {}
+A:""".format(message)
+
+response = lambda message: openai.Completion.create(
+        model="text-davinci-002",
+        prompt=get_message(message),
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\n"]
+        )
